9f6b194d0413b46b387a6b251d1f06025fb0f44d
Yaroslav Melnychenko <ym@empat.tech>
2025-09-15 13:25:54 +0300

    Updated CORS

--- api-function/index.js
+++ api-function/index.js
@@ -8,7 +8,7 @@ const sqs = new SQSClient({});
 
 const cors = {
     "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Headers": "Content-Type,Authorization",
+    "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
     "Access-Control-Allow-Methods": "OPTIONS,GET,POST,DELETE",
 };
 
